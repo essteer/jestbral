e913aec6e9c6b68f197cbdb42450d0749e543a20
essteer <essteer@pm.me>
2023-02-26 19:52:53 +0800

    add ejs-mate basic boilerplate

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const ejsMate = require("ejs-mate");
+const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
