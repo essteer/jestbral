3e3e4ea709bc4f1d87a89b7eb297fb8203677f87
essteer <essteer@pm.me>
2023-02-27 23:19:59 +0800

    add basic map and update style

--- public/javascripts/footprintsMap.js
+++ public/javascripts/footprintsMap.js
@@ -1,7 +1,15 @@
-mapboxgl.accessToken = mapboxToken;
+// mapboxgl.accessToken = mapboxToken;
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZXNzdGVlciIsImEiOiJjbGVsODd0bXYwc3Z6M3BtejQ4dWQ5Ym1rIn0.7t3-qqeuaVTtxLasNWpM_w";
+// const bounds = [
+//   [113.535, 21.85], // Southwest coordinates
+//   [114.85, 22.85], // Northeast coordinates
+// ];
+
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  style: "mapbox://styles/essteer/clecyji2s000c01msfsf6ldlv", // style URL
+  center: [114.136253, 22.356514], // starting position [lng, lat]
   zoom: 9, // starting zoom
+  // maxBounds: bounds,
 });
