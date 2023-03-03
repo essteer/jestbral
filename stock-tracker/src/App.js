44797b684a94a06560cc16bfe1a38d7a3e75671a
essteer <essteer@pm.me>
2023-03-03 13:17:20 +0800

    add basic stylesheet thead

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,8 +1,23 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./main.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Ticker</th>
+              <th>Price</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
